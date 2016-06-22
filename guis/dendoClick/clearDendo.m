464ca92beb4f9c01bd4a1edd0393ea17b9f031e0
CMSultana <camillesultana@gmail.com>
2016-06-22 14:44:03 -0700

    remove old gui
    got rid of old gui scripts that will no longer be using

--- guis/dendoClick/clearDendo.m
+++
@@ -1,7 +0,0 @@
-function clearDendo
-%CLEARDENDO clears all the global variables associated with
-%setup_clickDendo and clickDendo.  This frees up memory. 
-
-clear global dendoSPEC dendoLogSPEC dendoSPECavg dendoSIM dendoMZ dendoTime dendoSize dendoTimeBin dendoSizeBin linkCluster dendoCOUNTS Xlines Ylines finalbaseCluster lineCluster plotTrack rownum 
-
-
